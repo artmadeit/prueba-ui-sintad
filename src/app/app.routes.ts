f4a1237d9a140fdd439e4b767d274fc96e21fd8c
artmadeit <this.arthur.email@gmail.com>
2024-10-09 09:46:10 -0500

    feat: stub login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    // { path: 'second-component', component: SecondComponent },
+    { path: '',   redirectTo: '/login', pathMatch: 'full' },
+];
