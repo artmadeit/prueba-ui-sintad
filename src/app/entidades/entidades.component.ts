7a52582fa8978c1718f735330953929d13c70574
artmadeit <this.arthur.email@gmail.com>
2024-10-09 19:54:54 -0500

    style: add button and space between idea

--- src/app/entidades/entidades.component.ts
+++ src/app/entidades/entidades.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table'; 
 import { Entidad } from './entidad';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-entidades',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './entidades.component.html',
   styleUrl: './entidades.component.scss'
 })
