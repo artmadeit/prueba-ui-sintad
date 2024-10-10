1e509c17f54f569004896e7203998d6b5f06d4a0
artmadeit <this.arthur.email@gmail.com>
2024-10-09 22:06:53 -0500

    feat: setup jwt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { tokenGetter } from './token-getter';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { JwtModule } from "@auth0/angular-jwt";
+
+const baseUrl = "localhost:3000"
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: [baseUrl],
+          disallowedRoutes: [`${baseUrl}/auth/login`],
+        },
+      }),
+    ),
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+  ]
 };
