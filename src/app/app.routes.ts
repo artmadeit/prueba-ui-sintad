b4b75cc9bbc60411eca2116753b08192e880e022
artmadeit <this.arthur.email@gmail.com>
2024-10-10 01:20:21 -0500

    feat: add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { EntidadListComponent } from './entidad/entidad-list/entidad-list.compon
 import { PortalComponent } from './portal/portal.component';
 import { TipoContribuyenteListComponent } from './tipo-contribuyente/tipo-contribuyente-list/tipo-contribuyente-list.component';
 import { TipoDocumentoListComponent } from './tipo-documento/tipo-documento-list/tipo-documento-list.component';
+import { AuthGuard } from './login/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     {
         path: "portal",
         component: PortalComponent,
+        canActivate: [AuthGuard], 
         children: [
             { path: 'entidades', component: EntidadListComponent },
             { path: 'tipo-documentos', component: TipoDocumentoListComponent },
